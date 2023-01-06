b6ccf4bd5038567820a0d65a17800f6719019096
MichaelBoyo <boyomichaelbidemi@gmail.com>
2023-01-06 23:02:37 +0100

    all test passed for appointment service

--- src/test/java/com/boyo/healthrecords/services/AppointmentServiceImplTest.java
+++ src/test/java/com/boyo/healthrecords/services/AppointmentServiceImplTest.java
@@ -2,6 +2,7 @@ package com.boyo.healthrecords.services;
 
 import com.boyo.healthrecords.data.dataTransferObjects.requests.BookAppointmentRequest;
 import com.boyo.healthrecords.data.dataTransferObjects.requests.RegisterPatientRequest;
+import com.boyo.healthrecords.data.dataTransferObjects.responses.BookAppointmentResponse;
 import com.boyo.healthrecords.exceptions.PatientNotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,11 +18,15 @@ class AppointmentServiceImplTest {
     private AppointmentService appointmentService;
     @Autowired
     private PatientService patientService;
+    BookAppointmentResponse response;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws PatientNotFoundException {
         patientService.registerPatient(
                 new RegisterPatientRequest("John", "Doe", "09-10-1996", "test@gmail.com", "WeakPassword123")
+        );
+        response = appointmentService.bookAppointment(
+                new BookAppointmentRequest(1L)
         );
     }
 
@@ -30,15 +35,14 @@ class AppointmentServiceImplTest {
     }
 
     @Test
-    void bookAppointment() throws PatientNotFoundException {
-        appointmentService.bookAppointment(
-                new BookAppointmentRequest(1L)
-        );
+    void bookAppointment() {
+        assertEquals("Appointment booked successfully",
+                response.getMessage());
 
-        assertEquals(1, appointmentService.getPatientAppointments(1L).size());
     }
 
     @Test
     void getPatientAppointments() {
+        assertEquals(1, appointmentService.getPatientAppointments(1L).size());
     }
 }
